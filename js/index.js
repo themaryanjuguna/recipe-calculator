5d6c87aac267b82cd1d5e98858a6eabf5879512a
themaryanjuguna <themaryanjuguna@gmail.com>
2022-03-08 23:43:43 +0300

    updated index.php

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 let btn = document.getElementById('button');
 
 btn.addEventListener('click', function(){
-    let premixcost = document.getElementById('').value;
-    let costperkg = document.getElementById('').value;
+    let premixcost = document.getElementById('premix').value;
+    let costperkg = document.getElementById('premixcpk').value;
  
     
     let recipecost = document.getElementById('').value = premixcost*1000/costperkg;
